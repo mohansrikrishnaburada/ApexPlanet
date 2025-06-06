1a2a13554035d93d8512e2e3fd7a08334cfdb4b0
Burada Mohan Sri Krishna <mohansrikrishnaedu@gmail.com>
2025-06-06 12:08:00 +0530

    Add files via upload

---
+++ 1.Recipie Book/Rec.js
@@ -0,0 +1,15 @@
+function showMessage() {
+    alert("Happy Cooking!");
+}
+
+function toggleRecipe(button) {
+    const recipe = button.parentElement;
+    const details = recipe.querySelector('.recipe-details');
+    if (details.classList.contains('hidden')) {
+        details.classList.remove('hidden');
+        button.textContent = 'Hide Details';
+    } else {
+        details.classList.add('hidden');
+        button.textContent = 'Show Details';
+    }
+}
